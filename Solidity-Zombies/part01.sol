7623cd709aa6a793f27e02ed8bf4ba8fc7ae7092
SouL-H <mhmtaydinw@gmail.com>
2021-07-19 18:44:09 +0300

    Struct create, function create, Array push

--- Solidity-Zombies/part01.sol
+++ Solidity-Zombies/part01.sol
@@ -7,5 +7,18 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     //Chapper 4
     uint dnaModulus = 10 ** dnaDigits;
+    //Creating a struct
+    struct Zombie {
+            string name;
+            uint dna;
+    }
+    //Dynamic zombie array
+    Zombie[] public zombies;
+    //Creating a function 
+    //memory _name: specifies where the name will be provided in memory
+    function createZombie (string memory _name, uint _dna) public {
+        zombies.push(Zombie(_name, _dna)); // Array push
+    }
 
 }
+
